883dd7b154850f8cad394d545e5d3b49c7f7a166
Lei Zaakjyu <lizbing07734@icloud.com>
2023-11-11 19:46:29 +0800

    Fix 'young_gen' function in 'genCollectedHeap.cpp'

--- src/hotspot/share/gc/shared/genCollectedHeap.cpp
+++ src/hotspot/share/gc/shared/genCollectedHeap.cpp
@@ -25,6 +25,6 @@
 #include "gc/shared/genCollectedHeap.hpp"
 #include "gc/serial/serialHeap.hpp"
 
-inline Generation* GenCollectedHeap::young_gen() {
+Generation* GenCollectedHeap::young_gen() {
   return SerialHeap::heap()->young_gen();
 }
